da98793e530e3a96b88b5dfc3123d3d24ecdc82e
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2025-07-22 00:15:31 +0100

    Because this needs to be said.
    Signed-off-by: Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>

--- scripts/textAnimation.js
+++ scripts/textAnimation.js
@@ -1,3 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
+// I'll finish this one day.
+
+document.addEventListener('DOMContentLoaded', () => { 
     const dialogText = document.querySelectorAll('.dialogText');
 });
